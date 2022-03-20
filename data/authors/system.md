2bd25aff7e1838827e257c4d817d10b911000c13
TheRealToxicDev <toxic.dev09@gmail.com>
2022-03-20 05:17:56 -0600

    feat(add): Initial Commit

---
+++ data/authors/system.md
@@ -0,0 +1,10 @@
+---
+name: Sparrow Hawk
+avatar: /static/images/logo.png
+occupation: Staff Member
+company: DscJobs
+twitter: https://twitter.com/DscJobs
+linkedin: https://dscjobs.org
+---
+
+Default System profile for Staff Members who want to remain anonymous in our Blog Posts.
---
+++ tailwind.config.js
@@ -0,0 +1,187 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+/** @type {import("tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx', './lib/**/*.ts'],
+  theme: {
+    extend: {
+      spacing: {
+        '9/16': '56.25%',
+      },
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      },
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        '1a56db': '#1a56db',
+        breakcolor: 'var(--hr_color)',
+        headercolor: 'var(--headercolor)',
+        purewhite: 'var(--color)',
+        secondary_one: 'var(--secondary_color_one)',
+        secondary_two: 'var(--secondary_color_two)',
+        secondary_three: 'var(--secondary_color_three)',
+        secondary_four: 'var(--secondary_color_four)',
+        secondary_five: 'var(--secondary_color_five)',
+        secondary_six: 'var(--secondary_color_six)', 
+        disabled_color: 'var(--button_disabled)', 
+        def_sample: '#6476b8',
+        def_sample2: '#151823',
+        def_sample3: 'rgb(75, 83, 122)',
+        dis_sample: '#5866ef',
+        dis_sample2: '#36393f',
+        dis_sample3: '#7289DA',
+        dim_sample: '#4bb952',
+        dim_sample2: '#2e343d',
+        dim_sample3: '#2b682f',
+        drk_sample: '#f85d48',
+        drk_sample2: 'rgb(22, 22, 22)',
+        drk_sample3: '#fa452d88',
+        
+        primary: colors.teal,
+        //@ts-ignore
+        gray: colors.neutral,
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              color: 'var(--secondary_color_two)',
+              '&:hover': {
+                color: `var(--secondary_color_three) !important`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            h1: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.gray.900'),
+            },
+            h2: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.gray.900'),
+            },
+            h3: {
+              fontWeight: '600',
+              color: theme('colors.gray.900'),
+            },
+            'h4,h5,h6': {
+              color: theme('colors.gray.900'),
+            },
+            pre: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            code: {
+              color: theme('colors.pink.500'),
+              backgroundColor: theme('colors.gray.100'),
+              paddingLeft: '4px',
+              paddingRight: '4px',
+              paddingTop: '2px',
+              paddingBottom: '2px',
+              borderRadius: '0.25rem',
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+            details: {
+              backgroundColor: theme('colors.gray.100'),
+              paddingLeft: '4px',
+              paddingRight: '4px',
+              paddingTop: '2px',
+              paddingBottom: '2px',
+              borderRadius: '0.25rem',
+            },
+            hr: { borderColor: theme('colors.gray.200') },
+            'ol li::marker': {
+              fontWeight: '600',
+              color: theme('colors.gray.500'),
+            },
+            'ul li::marker': {
+              backgroundColor: theme('colors.gray.500'),
+            },
+            strong: { color: theme('colors.gray.600') },
+            blockquote: {
+              color: theme('colors.gray.900'),
+              borderLeftColor: theme('colors.gray.200'),
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: {
+              color: 'var(--secondary_color_two)',
+              '&:hover': {
+                color: `var(--secondary_color_three) !important`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            h1: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.gray.100'),
+            },
+            h2: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.gray.100'),
+            },
+            h3: {
+              fontWeight: '600',
+              color: theme('colors.gray.100'),
+            },
+            'h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+            pre: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            code: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            details: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            hr: { borderColor: theme('colors.gray.700') },
+            'ol li::marker': {
+              fontWeight: '600',
+              color: 'var(--secondary_color_two)',
+            },
+            'ul li::marker': {
+              backgroundColor: theme('colors.gray.400'),
+            },
+            strong: { color: theme('colors.gray.100') },
+            thead: {
+              th: {
+                color: theme('colors.gray.100'),
+              },
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme('colors.gray.700'),
+              },
+            },
+            blockquote: {
+              color: theme('colors.gray.100'),
+              borderLeftColor: theme('colors.gray.700'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
